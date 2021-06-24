bab147384c53a9a3f5707ed245105662da3c64c5
19WH1A1236-KP <85676088+19WH1A1236-KP@users.noreply.github.com>
2021-06-24 11:15:46 +0530

    Add files via upload

---
+++ JAVA Lab/Arrays/AddElem.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class AddElem
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n, i, sum = 0;
+System.out.println("Enter no. of elements: ");
+n = sc.nextInt();
+int a[] = new int[n];
+System.out.println("Enter the elements: ");
+for(i = 0; i < n; i++)
+{
+a[i] = sc.nextInt();
+}
+for(i = 0; i < n; i++)
+{
+sum += a[i];
+}
+System.out.println("Sum of elements = "+sum);
+}
+}
