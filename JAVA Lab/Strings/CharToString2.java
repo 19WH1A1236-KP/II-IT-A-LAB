7791a648d0a5f3579ad19d4bde480e1229646703
19WH1A1236-KP <85676088+19WH1A1236-KP@users.noreply.github.com>
2021-06-25 15:32:22 +0530

    Add files via upload

---
+++ JAVA Lab/Strings/CharToString2.java
@@ -0,0 +1,9 @@
+class CharToString2
+{
+   public static void main(String args[])
+   {
+      char ch = 'k';
+      String str = Character.toString(ch);
+      System.out.println("String is: "+str);
+   }
+}
