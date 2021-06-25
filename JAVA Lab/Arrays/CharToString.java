a17191422bcdde7b40311c674442aed91475d5e5
19WH1A1236-KP <85676088+19WH1A1236-KP@users.noreply.github.com>
2021-06-25 14:38:44 +0530

    Add files via upload

---
+++ JAVA Lab/Arrays/CharToString.java
@@ -0,0 +1,9 @@
+public class CharToString
+{
+   public static void main(String args[])
+   {
+      char[] ch = {'i','c','e','c','r','e','a','m'};
+      String str = new String(ch);
+      System.out.println(str);
+   }
+}
