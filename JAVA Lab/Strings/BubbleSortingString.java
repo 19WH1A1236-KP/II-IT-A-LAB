e51178961c767d579256bd7af57c477c9aeec064
19WH1A1236-KP <85676088+19WH1A1236-KP@users.noreply.github.com>
2021-07-02 14:11:08 +0530

    Add files via upload

---
+++ JAVA Lab/Strings/BubbleSortingString.java
@@ -0,0 +1,23 @@
+public class BubbleSortingString
+{
+   public static void main(String []args)
+   {
+	String str[] = { "Panda", "Lion", "Panther", "Leopard", "Penguin"};
+	String temp;
+	System.out.println("Strings in sorted order:");
+	for (int j = 0; j < str.length; j++) 
+  {
+   	   for (int i = j + 1; i < str.length; i++)
+       {
+		
+		if (str[i].compareTo(str[j]) < 0)
+    {
+			temp = str[j];
+			str[j] = str[i];
+			str[i] = temp;
+		}
+	   }
+	   System.out.println(str[j]);
+	}
+   }
+}
